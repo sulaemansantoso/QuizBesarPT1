51805fc363af446cb431f0d6dca2d4988af858e2
Sulaeman Santoso <sulaeman.santoso@outlook.com>
2020-10-26 13:59:26 +0700

    double commit

---
+++ src/Controller/Controller.java
@@ -0,0 +1,34 @@
+package Controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+
+public class Controller {
+    public Label lblName1;
+    public Label lblHP1;
+    public Label lblAccu1;
+    public Label lblName2;
+    public Label lblHp2;
+    public Label lblAccu2;
+    public ComboBox cmbMonster2;
+    public ComboBox cmbMonster1;
+
+    public void cmbMonster1(ActionEvent actionEvent) {
+    }
+
+    public void cmbMonster2(ActionEvent actionEvent) {
+    }
+
+    public void addNewMonster(ActionEvent actionEvent) {
+    }
+
+    public void addElement(ActionEvent actionEvent) {
+    }
+
+    public void fightAction2(ActionEvent actionEvent) {
+    }
+
+    public void fightAction1(ActionEvent actionEvent) {
+    }
+}
